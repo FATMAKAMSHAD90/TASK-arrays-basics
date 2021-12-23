6087b1fca8ddedf005ccd096061e72c4990f7c85
FATMAKAMSHAD90 <fatmakamshad90@gmail.com>
2021-12-23 10:06:00 +0300

    arrays done

---
+++ index.js
@@ -0,0 +1,27 @@
+let fruits = [];
+console.log(fruits);
+let hobbies = [
+  "Volleyball",
+  "Teniss",
+  "Eating",
+  "Dancing",
+  "Singing",
+  "Coding",
+];
+console.log(hobbies);
+
+let numbers = [4, 6, 8, 9, 10, 12];
+console.log(numbers);
+console.log("index2", numbers[1]);
+console.log("index5", hobbies[5]);
+hobbies.push("fake hobby");
+console.log(hobbies);
+console.log("index6", hobbies[6]);
+console.log(hobbies.length);
+hobbies.pop();
+console.log(hobbies);
+hobbies.pop();
+console.log(hobbies);
+console.log(hobbies.length);
+hobbies = [];
+console.log(hobbies);
